e6266a36fd8ec027fcb728bfe3e74884415aadd0
vishal911m <vishal911m@gmail.com>
2025-07-17 12:02:47 +0530

    Defining app routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
 
 const App = () => {
   return (
-    <div className='text-3xl text-purple-600'>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login /> } />
+          <Route path="/signUp" exact element={<SignUp /> } />
+          <Route path="/dashboard" exact element={<Home /> } />
+          <Route path="/income" exact element={<Income /> } />
+          <Route path="/expense" exact element={<Expense /> } />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
 export default App
+
+const Root = ()=>{
+  //Check if the token exists in the localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise login
+  return isAuthenticated ?(
+   <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
