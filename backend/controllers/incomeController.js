90de8c569b23ccaf44e1f359e14c5b13836f807c
vishal911m <vishal911m@gmail.com>
2025-07-18 15:33:25 +0530

    Creating Income APIs (Add Income, Get All Income, Delete Income, Download Excel Data)

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,76 @@
+const xlsx = require("xlsx");
+const Income = require("../models/Income");
+
+//Add income source
+exports.addIncome = async(req, res)=>{
+  const userId = req.user.id;
+
+  try {
+    const {icon, source, amount, date} = req.body;
+
+    // Validation: check for missing fields
+    if (!source || !amount || !date) {
+      return res.status(400).json({message: "All fields are required"})
+    }
+
+    const newIncome = new Income({
+      userId,
+      icon, 
+      source, 
+      amount, 
+      date: new Date(date)
+    });
+
+    await newIncome.save();
+    res.status(200).json(newIncome)
+  } catch (error) {
+    res.status(500).json({message: "Server error"})
+  }
+}
+
+//Get all income source
+exports.getAllIncome = async(req, res)=>{
+  const userId = req.user.id;
+
+  try {
+    const income = await Income.find({userId}).sort({date: -1});
+    res.json(income);
+  } catch (error) {
+    res.status(500).json({message: "Server error"})
+  }
+}
+
+//Delete income source
+exports.deleteIncome = async(req, res)=>{
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.json({message: "Income deleted successfully"});
+  } catch (error) {
+    res.status(500).json({message: "Server error"});
+  }
+}
+
+//Download Excel
+exports.downloadIncomeExcel = async(req, res)=>{
+  const userId = req.user.id;
+  
+  try {
+    const income = await Income.find({userId}).sort({date: -1});
+
+    //prepare data for excel
+    const data = income.map((item)=>({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.data,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_append_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, 'income_details.xlsx');
+    res.download('income_details.xlsx')
+  } catch (error) {
+    res.status(500).json({message: "Server error"});
+  }
+}
+
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes")
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
 
 // Server uploads folder
 app.use("uploads", express.static(path.join(__dirname, "uploads")));
