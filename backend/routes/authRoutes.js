34b820b43534d1e51056b89abe3c315d64bd836d
vishal911m <vishal911m@gmail.com>
2025-07-18 12:15:06 +0530

    Register and login route complete

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,19 @@
+const User = require("../models/User");
+const express = require("express");
+
+const {
+  registerUser,
+  loginUser,
+  getUserInfo,
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+// router.get("/getUser", protect, getUserInfo);
+
+module.exports = router;
+
