825be0612bff4acba6e4f4c98df7e2f8c37f71f0
vishal911m <vishal911m@gmail.com>
2025-07-20 10:58:07 +0530

    Fetching Dashboard Data via API Call

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -20,6 +20,31 @@ const Home = () => {
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const fetchDashboardData = async () =>{
+    if(loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if(response.data){
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.", error)
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    fetchDashboardData()
+    return ()=>{}
+  },[])
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className='my-5 mx-auto'>
