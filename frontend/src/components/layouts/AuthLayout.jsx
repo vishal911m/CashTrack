66b7231d234e6c899c1b50bd2662737e156c3917
vishal911m <vishal911m@gmail.com>
2025-07-23 11:35:05 +0530

    App Naming Updated

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = ({children}) => {
   return (
     <div className='flex'>
       <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-        <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+        <h2 className='text-lg font-medium text-black'>CashTrack App</h2>
         {children}
       </div>
 
